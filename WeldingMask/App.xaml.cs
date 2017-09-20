090d5a8775722d60fbc6e0838f4c64560796c716
Vaibhav Khanna <y2kvaibhav@gmail.com>
2017-09-20 19:59:32 +0530

    Setup Demo

---
+++ WeldingMask/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Xamarin.Forms;
+using FreshMvvm;
+using WeldingMask.PageModels;
+
+namespace WeldingMask
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var page = FreshPageModelResolver.ResolvePageModel<CameraPageModel>();
+            var container = new FreshNavigationContainer(page){ BarTextColor = Color.White, BarBackgroundColor = Color.Black };
+
+            MainPage = container;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ WeldingMask/PageModels/CameraPageModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using WeldingMask.PageModels.Base;
+using Xamarin.Forms;
+
+namespace WeldingMask.PageModels
+{
+    public class CameraPageModel : BasePageModel
+    {
+       
+        public Command ScreenTap => new Command(() =>
+       {
+           
+       });
+
+
+        bool control_visible = true;
+        public bool IsControlVisible
+        {
+            get { return control_visible; }
+            set
+            {
+                control_visible = value;
+
+
+                RaisePropertyChanged();
+            }
+        }
+
+
+        double opacity;
+        public double FilterOpacity
+        {
+            get { return opacity; }
+            set
+            {
+                opacity = value;
+
+                RaisePropertyChanged();
+            }
+        }
+
+
+
+
+        public override void Init(object initData)
+        {
+            base.Init(initData);
+
+            FilterOpacity = 0.2;
+           
+        }
+
+
+    }
+}
